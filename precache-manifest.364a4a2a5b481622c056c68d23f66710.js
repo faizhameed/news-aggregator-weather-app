6d7dcdea48b79c89100c56c413c10215c8001910
Faiz Hameed <faizftb@gmail.com>
2020-06-16 09:46:10 +0530

    Updates

---
+++ precache-manifest.364a4a2a5b481622c056c68d23f66710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb843ff4015234a59177861ce8df2e4",
+    "url": "/news-aggregator-weather-app/index.html"
+  },
+  {
+    "revision": "844d05dcc77d2591f298",
+    "url": "/news-aggregator-weather-app/static/css/main.9173300d.chunk.css"
+  },
+  {
+    "revision": "95cdea4d0b7ea3e05879",
+    "url": "/news-aggregator-weather-app/static/js/2.b3a73bc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/news-aggregator-weather-app/static/js/2.b3a73bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844d05dcc77d2591f298",
+    "url": "/news-aggregator-weather-app/static/js/main.0e034043.chunk.js"
+  },
+  {
+    "revision": "3014e902ea4b9c555683",
+    "url": "/news-aggregator-weather-app/static/js/runtime-main.d326df62.js"
+  }
+]);
