7cbbdab1149e9fe2c0a725369788bd12f81042a8
Faiz Hameed <faizftb@gmail.com>
2020-10-29 21:43:59 +0530

    geolocation api with redux saga

--- src/redux/geoLocation/actions.js
+++ src/redux/geoLocation/actions.js
@@ -1,48 +1,44 @@
 import { geoLocationTypes } from "./types";
 import Axios from "axios";
-export function getGeoLocation() {
-  /* if ("geolocation" in navigator) {
-    let coordinates = {};
-    navigator.geolocation.getCurrentPosition(function (position) {
-      coordinates["latitude"] = position.coords.latitude;
-      coordinates["longitude"] = position.coords.longitude;
-    });
+// export function getGeoLocation() {
+//   return (dispatch) => {
+//     dispatch({
+//       type: geoLocationTypes.GET_GEO_LOCATION_PENDING,
+//     });
+//     const geolocation = navigator.geolocation;
+// geolocation.getCurrentPosition((position) => {
+//   console.log(position.coords);
+//   dispatch({
+//     type: geoLocationTypes.GET_GEO_LOCATION_SUCCESS,
+//     payload: {
+//       latitude: position.coords.latitude,
+//       longitude: position.coords.longitude,
+//     },
+//   });
+// });
+//     if (!navigator.geolocation) {
+//       dispatch({
+//         type: geoLocationTypes.GET_GEO_LOCATION_FAILED,
+//       });
+//     }
 
-    return {
-      type: geoLocationTypes.GET_GEO_LOCATION_SUCCESS,
-      payload: coordinates,
-    };
-  } else {
-    return {
-      type: geoLocationTypes.GET_GEO_LOCATION_FAILED,
-    };
-  } */
-  return (dispatch) => {
-    dispatch({
-      type: geoLocationTypes.GET_GEO_LOCATION_PENDING,
-    });
-    const geolocation = navigator.geolocation;
-    geolocation.getCurrentPosition((position) => {
-      console.log(position.coords);
-      dispatch({
-        type: geoLocationTypes.GET_GEO_LOCATION_SUCCESS,
-        payload: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        },
-      });
-    });
-    if (!navigator.geolocation) {
-      dispatch({
-        type: geoLocationTypes.GET_GEO_LOCATION_FAILED,
-      });
-    }
-    /* .catch((error) =>
-        
-      ); */
-  };
-}
+//   };
+// }
 
+export const getGeoLocation = () => ({
+  type: geoLocationTypes.GET_GEO_LOCATION_PENDING,
+});
+export const receiveGeoLocation = ({ latitude, longitude }) => ({
+  type: geoLocationTypes.GET_GEO_LOCATION_SUCCESS,
+  payload: {
+    latitude,
+    longitude,
+  },
+});
+
+export const failedGeoLocation = () => ({
+  type: geoLocationTypes.GET_GEO_LOCATION_FAILED,
+});
 export function getWeatherReport({ latitude, longitude }) {
   console.log("latitude", latitude);
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=48e1d541b5f45f74de028c9c43d25296`;
